2335f6751377220bcea549dbe70daa68472a55af
jbatnozic <jovanbatnozic@hotmail.rs>
2025-02-12 01:49:52 +0100

    Implement simple Unicode-aware regex class

--- EngineCore/Modules_L00_Foundation/S04_Unicode/Include/Hobgoblin/Unicode.hpp
+++ EngineCore/Modules_L00_Foundation/S04_Unicode/Include/Hobgoblin/Unicode.hpp
@@ -6,6 +6,7 @@
 #include <Hobgoblin/Unicode/Charset_detection.hpp>
 #include <Hobgoblin/Unicode/File_io.hpp>
 #include <Hobgoblin/Unicode/Fmt_compat.hpp>
+#include <Hobgoblin/Unicode/Regex.hpp>
 #include <Hobgoblin/Unicode/String_conversions.hpp>
 #include <Hobgoblin/Unicode/Unicode_string.hpp>
 
